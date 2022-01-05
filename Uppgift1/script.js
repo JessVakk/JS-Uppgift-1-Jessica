f0df08f7e5f0a78b392bf1afcfa988dfef1b059b
Jessica Vakkuri <jessica.vakkuri@utb.ecutbildning.se>
2022-01-05 16:43:25 +0100

    tagit bort kommentarer

--- Uppgift1/script.js
+++ Uppgift1/script.js
@@ -43,7 +43,7 @@ const validateEmail = (emailInput) => {
 
 
 const lista= []
-//lägger grejern i den ovanför
+
 const listUser = () => {
     output.innerHTML = '';    //tömmer listan och skriver om den så att det inte blir dubletter
     lista.forEach(user =>{
@@ -64,26 +64,7 @@ regForm.addEventListener('submit', (e) =>{
     e.preventDefault();
 //gör så att inget skrivs ut om det är ett tomt fält
 
-// if(firstName.value !=='' && lastName.value !=='' && email.value){ //om fältet inte är tomt
-//     firstName.classList.remove('is-invalid');
-//        const user={
-//         id:Date.now().toString(),
-//         firstName: (firstName.value ) + '  ' + ( lastName.value),
-//         email: email.value,
-//         completed: false
-//     }
-//     lista.push(user);
-//     listUser();
-//     firstName.value = '' //tömmer input-rutan
-//     lastName.value = ''
-//     email.value = '' 
-    
-// } else {
-//     firstName.classList.add('is-invalid');
-// }
 
-// regForm.addEventListener('submit', e => {
-//   e.preventDefault();
 
  
   const errors = [];
@@ -121,9 +102,6 @@ regForm.addEventListener('submit', (e) =>{
     email.value = ''
      
     }
-
-
-
     
 })
 
